c4916427a554820c647686c62c6f544013a3c43c
mars-monkey <91227993+mars-monkey@users.noreply.github.com>
2024-02-17 11:21:02 +0100

    Local changes autocommit

--- home.nix
+++ home.nix
@@ -211,7 +211,7 @@
         "$mod, R, exec, $menu"
         "$mod, P, pseudo,"
         "$mod, O, togglesplit,"
-        ", Print, grimblast copy"
+        ", Print, exec, grimblast copy"
         
         "$mod, H, movefocus, l"
         "$mod, L, movefocus, r"
