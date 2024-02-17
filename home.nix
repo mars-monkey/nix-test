9bf034ea4b3c44c1820dab46f78831c164a5056d
mars-monkey <91227993+mars-monkey@users.noreply.github.com>
2024-02-17 09:32:39 +0100

    Local changes autocommit

--- home.nix
+++ home.nix
@@ -225,34 +225,25 @@
         "$mod, 9, workspace, 9"
         "$mod, 0, workspace, 10"
 
-        /*"$mod, 
-        "$mod, 
-        "$mod, 
-        "$mod, 
-        "$mod, 
-        "$mod, 
-        "$mod, 
-        "$mod, 
-        "$mod, 
-        "$mod, 
-        "$mod, 
-        "$mod, 
-        "$mod, 
-        "$mod, 
-        "$mod, 
-        "$mod, 
-        "$mod, 
-        "$mod, 
-        "$mod, 
-        "$mod, 
-        "$mod, 
-        "$mod, 
-        "$mod, 
-        "$mod, 
-        "$mod, 
-        "$mod, 
-        "$mod, 
-        "$mod,        */
+        "$mod SHIFT, 1, movetoworkspace, 1"
+        "$mod SHIFT, 2, movetoworkspace, 2" 
+        "$mod SHIFT, 3, movetoworkspace, 3" 
+        "$mod SHIFT, 4, movetoworkspace, 4"
+        "$mod SHIFT, 5, movetoworkspace, 5"
+        "$mod SHIFT, 6, movetoworkspace, 6"
+        "$mod SHIFT, 7, movetoworkspace, 7"
+        "$mod SHIFT, 8, movetoworkspace, 8"
+        "$mod SHIFT, 9, movetoworkspace, 9"
+        "$mod SHIFT, 0, movetoworkspace, 10"
+
+        "$mod, S, togglespecialworkspace, magic"
+        "$mod SHIFT, S, movetoworkspace, special:magic"
+        "$mod, mouse_down, workspace, e+1"
+        "$mod, mouse_up, workspace, e-1"
+        "$mod, mouse:272, movewindow"
+        "$mod, mouse:273, resizewindow"
+        "$mod, xf86audioraisevolume, exec, wpctl set-volume -l 1.0 @DEFAULT_SINK@ 5%+"
+        "$mod, xf86audiolowervolume, exec, wpctl set-volume -l 1.0 @DEFAULT_SINK@ 5%-"
       ];
     };
   };
