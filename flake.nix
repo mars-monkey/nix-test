53ddd4a37d9ca338497e3ba9a92dd671d34e9a6c
mars-monkey <91227993+mars-monkey@users.noreply.github.com>
2024-03-17 15:01:50 +0100

    Local changes autocommit

--- flake.nix
+++ flake.nix
@@ -29,7 +29,7 @@
     };
   };
 
-  nixConfig = {
+  /*nixConfig = {
     substituters = [
       "https://cache.nixos.org"
       "https://nix-community.cachix.org"
@@ -41,5 +41,5 @@
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
     ];
-  };
+  };*/
 }
