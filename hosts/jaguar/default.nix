0ac0cc1531075ed45a8db41435e9b822d0078df5
mars-monkey <91227993+mars-monkey@users.noreply.github.com>
2024-08-14 10:29:17 -0700

    Local changes autocommit

--- hosts/jaguar/default.nix
+++ hosts/jaguar/default.nix
@@ -188,7 +188,7 @@
       servers = {
         test = {
 	  enable = true;
-	  #package = pkgs.fabricServers.fabric-1_18_2;
+	  package = pkgs.fabricServers.fabric-1_18_2;
 	  /*jvmOpts = "-Xms4092M -Xmx4092M -XX:UseG1GC nogui";
 
 	  serverProperties = {
